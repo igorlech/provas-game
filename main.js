dfcaa718e925e8f416882854ef7fa391b2b6c747
Igor Lech <igl-ext@illumi.dk>
2023-05-24 12:04:03 +0200

    initial drag and drop logic using gsap

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 import "./styles/style.scss";
 import "./styles/reset.scss";
+
+import { gsap } from "gsap";
+import { Draggable } from "gsap/Draggable";
+
+gsap.registerPlugin(Draggable);
+
+function createDraggable() {
+  Draggable.create(".box", { bounds: ".draggable" });
+  console.log("draggable component loaded");
+}
+
+function gameInit() {
+  createDraggable();
+}
+
+gameInit();
