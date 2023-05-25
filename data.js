cf22e1fa276cc9b5c1faad8720102b8f21d52412
Igor Lech <igl-ext@illumi.dk>
2023-05-25 10:36:06 +0200

    items random spawn, drag&drop fix, data file

---
+++ data.js
@@ -0,0 +1,17 @@
+export const images = [
+  {
+    src: "./assets/Clutery.svg",
+    id: "clutery",
+    alt: "cutlery",
+  },
+  {
+    src: "./assets/Banana.svg",
+    id: "banana",
+    alt: "banana",
+  },
+  {
+    src: "./assets/Bread.svg",
+    id: "bread",
+    alt: "bread",
+  },
+];
--- main.js
+++ main.js
@@ -1,50 +1,73 @@
 import "./styles/style.scss";
 import "./styles/reset.scss";
-
-// function init() {
-//   function dragOver(ev) {
-//     ev.preventDefault();
-//   }
-
-//   function dragStart(ev) {
-//     ev.dataTransfer.setData("text", ev.target.id);
-//   }
-
-//   function drop(ev) {
-//     ev.preventDefault();
-//     let data = ev.dataTransfer.getData("text");
-//     ev.target.appendChild(document.getElementById(data));
-//   }
-// }
-
-// init();
+import "./data.js";
+import { images } from "./data.js";
 
 console.log("main.js loaded");
 
 // Get references to the draggable and droppable elements
 const draggables = document.querySelectorAll(".draggable");
 const droppables = document.querySelectorAll(".droppable");
+const items = document.querySelectorAll(".box");
+const rubbishSection = document.querySelector(".rubbish-section");
 
-// Define the dragstart event handler for the draggable elements
-draggables.forEach((draggable) => {
-  draggable.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("text/plain", event.target.id);
+function startDraggable() {
+  // Define the dragstart event handler for the draggable elements
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", (event) => {
+      event.dataTransfer.setData("text/plain", event.target.id);
+    });
   });
-});
 
-// Define the dragover event handler for the droppable elements
-droppables.forEach((droppable) => {
-  droppable.addEventListener("dragover", (event) => {
-    event.preventDefault();
+  // Define the dragover event handler for the droppable elements
+  droppables.forEach((droppable) => {
+    droppable.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
   });
-});
 
-// Define the drop event handler for the droppable elements
-droppables.forEach((droppable) => {
-  droppable.addEventListener("drop", (event) => {
-    event.preventDefault();
-    const data = event.dataTransfer.getData("text/plain");
-    const draggableElement = document.getElementById(data);
-    droppable.appendChild(draggableElement);
+  // Define the drop event handler for the droppable elements
+  droppables.forEach((droppable) => {
+    droppable.addEventListener("drop", (event) => {
+      event.preventDefault();
+      const data = event.dataTransfer.getData("text/plain");
+      const draggableElement = document.getElementById(data);
+      droppable.appendChild(draggableElement);
+    });
   });
-});
+}
+
+function spawnItems() {
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.top =
+      Math.floor(Math.random() * (window.innerHeight - 100)) + "px";
+    items[i].style.right =
+      Math.floor((Math.random() * window.innerWidth - 100) / 2) + 100 + "px";
+  }
+}
+
+function renderItems() {
+  images.forEach((image) => {
+    rubbishSection.innerHTML += `
+      <div class="box" draggable="true">
+      <img
+        class="rubbish draggable"
+        src=${image.src}
+        draggable="true"
+        id=${image.id}
+        alt=${image.alt}
+      />
+      </div>
+    `;
+  });
+}
+
+// rubbishSection.insertAdjacentHTML("beforeend", );
+
+function gameInit() {
+  startDraggable();
+  // renderItems();
+  spawnItems();
+}
+
+gameInit();
